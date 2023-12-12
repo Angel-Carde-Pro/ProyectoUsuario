69ae79fca08fd79d33fad0d49eb06cf5fa9a7d48
AngelInsta <angel.cardenas.est@tecazuay.edu.ec>
2023-12-12 09:22:07 -0500

    Se subio la configuración del application

--- src/test/java/com/example/ProyectoUsuarioSpring/ProyectoUsuarioSpringApplicationTests.java
+++ src/test/java/com/example/ProyectoUsuarioSpring/ProyectoUsuarioSpringApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ProyectoUsuario;
+package com.example.ProyectoUsuarioSpring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProyectoUsuarioApplicationTests {
+class ProyectoUsuarioSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
